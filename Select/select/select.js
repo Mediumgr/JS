1ea0211a9c1b58a08c8c3b9fa97bbf14c5d05873
Руслан <Mediumgr@yandex.ru>
2023-01-13 01:42:43 +0300

    start to make plugin

---
+++ Select/select/select.js
@@ -0,0 +1,11 @@
+const getTemplate = () => {
+  return `<div class="select__input" data-type="input">fdfsdfs</div>
+          <div class="select__dropdown">
+            <ul class="select__list">
+              <li class="select__item"></li>
+              <li class="select__item"></li>
+            </ul>
+          </div>`;
+};
+
+export class Select {}
